3afec27acff823df0bccff4c45aa6cb759eec075
Karen Jiang <kkarenjjiang.com>
2023-07-30 13:38:45 -0700

    form

--- my-app/src/Components/AddPost/AddPost.jsx
+++ my-app/src/Components/AddPost/AddPost.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import Form, { Input, FormButton, TextArea } from "react-form-component";
-import { Row } from "./styled-components";
+import Form, {
+  Input,
+  TextArea,
+  SubmitButton,
+  FormActions,
+} from "react-form-component";
+import { CancelButton } from "./styled-components";
 
 function AddPost() {
   return (
@@ -15,10 +20,12 @@ function AddPost() {
         placeholder='Separate ingredients with a comma'
       />
       <TextArea name='notes' label='Notes' rows='5' />
-      <Row>
-        <FormButton onClick={(fields) => console.log(fields)}>Save</FormButton>
-        <FormButton>cancel</FormButton>
-      </Row>
+      <FormActions align='left'>
+        <SubmitButton onClick={(fields) => console.log(fields)}>
+          Save
+        </SubmitButton>
+        <CancelButton>Cancel</CancelButton>
+      </FormActions>
     </Form>
   );
 }
--- my-app/src/Components/AddPost/styled-components.jsx
+++ my-app/src/Components/AddPost/styled-components.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
-export const Row = styled.div`
-display: flex;
-flex-direction row;
-gap: 10px;
+export const CancelButton = styled.button`
+  background-color: #1565c0;
+  border: none;
+  color: white;
+  padding: 0px 20px;
+  line-height: 40px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  &:hover {
+    background-color: #217ee7;
+  }
 `;
